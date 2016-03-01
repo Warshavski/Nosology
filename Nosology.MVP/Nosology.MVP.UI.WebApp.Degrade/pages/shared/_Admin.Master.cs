65c62e7d7440fd433ce145c102acf066753143a7
Warshavski <shikari005@gmail.com>
2016-03-01 17:44:54 +0300

    Added : admin pages signout

--- Nosology.MVP/Nosology.MVP.UI.WebApp.Degrade/pages/shared/_Admin.Master.cs
+++ Nosology.MVP/Nosology.MVP.UI.WebApp.Degrade/pages/shared/_Admin.Master.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -12,6 +13,28 @@ namespace Nosology.MVP.UI.WebApp.Degrade.pages.shared
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            var usr = Session["User"] as Escyug.Nosology.MVP.Engine.User;
+
+            if (usr != null)
+            {
+                this.userName.Text = usr.Name;
+                this.userLevel.Text = usr.Level;
+
+                this.signOutLink.ServerClick += (send, args) =>
+                {
+                    FormsAuthentication.SignOut();
+                    FormsAuthentication.RedirectToLoginPage();
+                };
+            }
+            else
+            {
+                FormsAuthentication.SignOut();
+                FormsAuthentication.RedirectToLoginPage();
+            }
         }
     }
 }
